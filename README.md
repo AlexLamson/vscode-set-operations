1a340511673cbbf9999bb94c6bd61e7255609e65
Alex Lamson <alex.lamson@gmail.com>
2023-01-16 10:12:15 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Set Operations Extension for VSCode
+
+This extension allows you to perform set operations (such as intersection and difference) on a pair of user-selected tabs in the VSCode editor.
+
+## Features
+* Intersection: returns the common elements between the two selected sets.
+* Difference: returns the elements in the first set that are not in the second set.
+* Symmetric Difference: returns all elements that are not in the common set.
+* Union: returns all elements of both sets
+
+## Usage
+1. Open the command palette and search for the operation you would like to perform (eg. intersection, difference, etc.)
+1. Choose the first file
+1. Choose the second file
+1. The result of the operation will be written to a new tab
+
+## Note
+* This extension only works with text that is separated by newlines
+* The result of a set operation has no ordering guarantees
+
+## Contribution
+Please feel free to contribute to this extension by submitting pull requests or by reporting bugs.
+Support
+
+If you have any questions or issues, please open an issue on the [GitHub page](https://github.com/AlexLamson/vscode-set-operations) for this extension.
---
+++ src/extension.ts
@@ -0,0 +1,103 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { window, commands, ExtensionContext } from 'vscode';
+
+async function openInUntitled(content: string, language?: string) {
+    const document = await vscode.workspace.openTextDocument({
+        language,
+        content,
+    });
+    vscode.window.showTextDocument(document);
+}
+
+async function userSelectTab(filenames:string[], docs: readonly vscode.TextDocument[], step:Number): Promise<string[]> {
+    const filename = await window.showQuickPick(filenames, {
+		placeHolder: `Choose the file (Step ${step}/3)`
+	});
+	if(filename === undefined) {
+		return [];
+	}
+	const tab = docs[filenames.indexOf(filename)];
+	const lines = tab.getText().split('\n');
+	// window.showInformationMessage(`Got: ${filename}`);
+	return lines;
+}
+
+async function getTwoTabs(): Promise<[string[], string[]]> {
+
+	const docs = vscode.workspace.textDocuments;
+
+	const filenames = docs.map(doc => doc.fileName);
+
+	// Select the first file
+	const lines1 = await userSelectTab(filenames, docs, 1);
+
+	// Select the second file
+	const lines2 = await userSelectTab(filenames, docs, 2);
+
+	return new Promise((resolve) => {
+		resolve([lines1, lines2]);
+	});
+}
+
+function stableSymmetricDifference(lines1:string[], lines2:string[]):string[] {
+	// TODO make this stable
+	const set1 = new Set(lines1);
+	const set2 = new Set(lines2);
+	const outputSet = new Set(set1);
+	var output = Object.assign([], lines1);
+
+    for (const elem of lines2) {
+        const operation = (outputSet.has(elem)) ? 'delete' : 'add';
+        outputSet[operation](elem);
+		// const operation = (outputSet.has(elem)) ? 'delete' : 'add';
+        // output[operation](elem);
+    }
+
+    return Array.from(outputSet);
+}
+
+export function activate(context: ExtensionContext) {
+	context.subscriptions.push(commands.registerCommand('set-operations.intersection', async () => {
+		const [lines1, lines2] = await getTwoTabs();
+
+		const set1 = new Set(lines1);
+		const set2 = new Set(lines2);
+		let setIntersection = lines1.filter(x => set2.has(x));
+
+		openInUntitled(setIntersection.join("\n"));
+	}));
+
+	context.subscriptions.push(commands.registerCommand('set-operations.difference', async () => {
+		const [lines1, lines2] = await getTwoTabs();
+
+		const set1 = new Set(lines1);
+		const set2 = new Set(lines2);
+		let setDifference = lines1.filter(x => !set2.has(x));
+
+		openInUntitled(setDifference.join("\n"));
+	}));
+
+	context.subscriptions.push(commands.registerCommand('set-operations.symmetric_difference', async () => {
+		const [lines1, lines2] = await getTwoTabs();
+
+		let setSymmetricDifference = stableSymmetricDifference(lines1, lines2);
+
+		openInUntitled(setSymmetricDifference.join("\n"));
+	}));
+
+	context.subscriptions.push(commands.registerCommand('set-operations.union', async () => {
+		const [lines1, lines2] = await getTwoTabs();
+
+		let setUnion = new Set([...lines1, ...lines2]);
+
+		openInUntitled(Array.from(setUnion).join("\n"));
+	}));
+
+	// TODO publish extension
+}
+
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
